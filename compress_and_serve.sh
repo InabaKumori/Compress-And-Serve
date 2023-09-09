af23ae3654fda97b94276f479a82517a4f58473d
WereIYou <51642034+InabaKumori@users.noreply.github.com>
2023-09-09 10:32:31 +0800

    Update compress_and_serve.sh

--- compress_and_serve.sh
+++ compress_and_serve.sh
@@ -6,37 +6,31 @@ get_public_ip() {
     echo "$PUBLIC_IP"
 }
 
-# Default to compressing the current directory
+# Default to serving the current directory
 if [ -z "$1" ]; then
-    TARGET="current_directory"
-    tar --exclude="$TARGET.tar.gz" -czf "$TARGET.tar.gz" .
-    COMPRESSED_TARGET="$TARGET.tar.gz"
+    TARGET="."
+    SERVE_TARGET="$TARGET"
 else
-    # Determine whether it's a file or directory and compress accordingly
+    # Determine whether it's a file or directory
     TARGET="$1"
     if [ -f "$TARGET" ]; then
-        tar -czf "$TARGET.tar.gz" "$TARGET"
-        COMPRESSED_TARGET="$TARGET.tar.gz"
+        # If it's a single file, there's no need to compress
+        SERVE_TARGET="$TARGET"
     elif [ -d "$TARGET" ]; then
+        # If it's a directory, compress it
         tar -czf "$TARGET.tar.gz" -C "$TARGET" .
-        COMPRESSED_TARGET="$TARGET.tar.gz"
+        SERVE_TARGET="$TARGET.tar.gz"
     else
         echo "The target is neither a file nor a directory. Exiting."
         exit 1
     fi
 fi
 
-# Check if the compressed target is created and not empty
-if [ ! -f "$COMPRESSED_TARGET" ] || [ ! -s "$COMPRESSED_TARGET" ]; then
-    echo "Failed to compress the target. Exiting."
-    exit 1
-fi
-
 # Get public IP address
 PUBLIC_IP=$(get_public_ip)
 
 # Display the link
-echo "Your target will be available at http://$PUBLIC_IP:3000/$COMPRESSED_TARGET"
+echo "Your target will be available at http://$PUBLIC_IP:3000/$SERVE_TARGET"
 
 # Start a simple HTTP server on port 3000
 python3 -m http.server 3000
